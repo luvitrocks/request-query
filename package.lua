ce669711d69ac1a33425138af0cd9b2833abf919
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-07-22 23:24:55 +0300

    add request query

---
+++ package.lua
@@ -0,0 +1,15 @@
+-- for older versions of luvit and alternative package managers
+return {
+	name = "luvit-request-query",
+	version = "0.0.4-beta",
+	description = "Automatically parse the querystring when available to `req.query` table",
+	repository = {
+		url = "http://github.com/luvitrocks/luvit-request-query.git",
+	},
+	author = {
+		name = "Dmitri Voronianski",
+		email = "dmitri.voronianski@gmail.com"
+	},
+	licenses = {"MIT"},
+	main = 'init.lua'
+}
